eb873161a98ebfe110177e56261f2273f9a16822
Leonardo Godoy <leonardo033@gmail.com>
2025-10-30 01:32:56 +0000

    update test component

--- packages/svelte-timeseries/src/routes/duckdb/+page.svelte
+++ packages/svelte-timeseries/src/routes/duckdb/+page.svelte
@@ -13,15 +13,14 @@
 	const timeSeries = new TimeSeriesChartBuilder();
 	let timeSeriesOption = $state<EChartsOption>({});
 
-	// let duckChart: DuckChart;
-	// let chart: SVECharts;
-
 	let loading = $state(true);
 
 	async function load() {
 		console.log('Load DuckDb...');
 		duckDb = await DuckDB.create(tables, debug);
-		const result = await duckDb.query(`SELECT _ts, price FROM signal WHERE price NOT NULL`);
+		const result = await duckDb.query(
+			`SELECT _ts, price FROM signal WHERE price NOT NULL LIMIT 50000`
+		);
 
 		const rows = result.toArray();
 
@@ -36,10 +35,6 @@
 			_ts: Number(r._ts)
 		}));
 		const table = [...normalized.map(Object.values)];
-
-		console.log('table loaded');
-
-		await tick();
 		timeSeries
 			.setTitle('Tempp', 'Last 24 hours')
 			.setDataset(table, yDimensions)
@@ -54,25 +49,7 @@
 
 	onMount(async () => {
 		await load();
-		// console.log('Mounting DuckDb...');
-		// duckDb = await DuckDB.create(tables, debug);
-		// 	duckChart = new DuckChart();
-		// 	duckChart.debug = debug;
-		// 	loadUrl(url);
 	});
-
-	// const loadUrl = async (url: string) => {
-	// 	if (!url || url === '') return;
-	// 	if (!duckChart) return;
-	// 	loading = true;
-	// 	await duckChart.initDB(url);
-	// 	await duckChart.load(url);
-	// 	duckChart.option = { ...duckChart.option }; // Force Svelte reactivity
-	// 	loading = false;
-	// };
-	// $: if (url) {
-	// 	loadUrl(url);
-	// }
 </script>
 
 <div class="container" style="height: 80vh;">
