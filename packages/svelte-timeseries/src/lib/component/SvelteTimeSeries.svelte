dac315150f88ee597bce438a61fe07039e370d02
Leonardo Godoy <leonardo033@gmail.com>
2025-11-07 12:46:21 +0000

    Updating sample files

--- packages/svelte-timeseries/src/lib/component/SvelteTimeSeries.svelte
+++ packages/svelte-timeseries/src/lib/component/SvelteTimeSeries.svelte
@@ -1,60 +1,65 @@
 <script lang="ts">
+	import { DuckDB, type Tables } from '$lib/duckdb/DuckDB';
+	import { type EChartsOption, SVECharts, TimeSeriesChartBuilder } from '@qtsurfer/sveltecharts';
 	import { onMount } from 'svelte';
-	import { SVECharts } from '@qtsurfer/sveltecharts';
-	import { DuckChart } from '$lib/DuckChart';
 
-	export let url: string;
-	export let debug: boolean = false;
-
-	let chart: SVECharts;
-	let duckChart: DuckChart;
-
-	let loading = false;
-	let changes = 0;
-
-	onMount(async () => {
-		console.log('Mounting SvelteTimeSeries...');
-		duckChart = new DuckChart();
-		duckChart.debug = debug;
-		loadUrl(url);
-	});
+	let { table, debug = true }: { table: Tables; debug: boolean } = $props();
 
 	/**
-	 * @todo Fix this
+	 * The `_m` value represents the column where *markers* are stored, and its value indicates the associated table name.
+	 * You can assign either a Parquet file or a URL.
+	 * If a column is used, it will search for that column across all available tables.
+	 * Note that only one *markers* column can exist across all tables.
 	 */
-	// const handleDataZoom = (e: CustomEvent) => {
-	// let start, end;
-	// if (e.detail.batch) {
-	// 	const [info] = e.detail.batch;
-	// 	start = info.start;
-	// 	end = info.end;
-	// } else {
-	// 	start = e.detail.start;
-	// 	end = e.detail.end;
-	// }
-	// duckChart.loadRange(start, end).then(() => {
-	// 	// Force Svelte reactivity
-	// 	// @ts-ignore
-	// 	duckChart.option.dataZoom[0] = { ...duckChart.option.dataZoom[0], start, end };
-	// 	duckChart.option.dataset = duckChart.option.dataset;
+	// let tables = $derived<Tables>({
+	// 	markers: {
+	// 		table: 'signal',
+	// 		targetColumn: '_m'
+	// 	},
+	// 	signal: url
+	//  query: 'SELECT _ts, price FROM signal WHERE price NOT NULL'
 	// });
-	// };
 
-	const loadUrl = async (url: string) => {
-		if (!url || url === '') return;
-		if (!duckChart) return;
+	let timeSeriesOption = $state<EChartsOption>({});
+	let changes = $state(1);
+	let loading = $state(false);
+
+	async function load() {
 		loading = true;
-		await duckChart.initDB(url);
-		await duckChart.load(url);
-		duckChart.option = { ...duckChart.option }; // Force Svelte reactivity
-		changes++;
+		let duckDb = await DuckDB.create(table, debug);
+		const timeSeries = new TimeSeriesChartBuilder(timeSeriesOption);
+		timeSeries.setLegendIcon('rect');
+
+		const result = await duckDb.query(table.query);
+		const rows = result.toArray();
+
+		timeSeries.setDataset(rows);
+
+		if (table.markers) {
+			const markers = await duckDb.getMarkers();
+			const markersRows = markers.toArray();
+			for (const m of markersRows) {
+				timeSeries.addMarkerPoint(
+					{
+						dimName: 'price',
+						timestamp: m._ts,
+						name: m.text
+					},
+					{
+						icon: m.shape,
+						color: m.color
+					}
+				);
+			}
+		}
 		loading = false;
-	};
-	$: if (url) {
-		loadUrl(url);
 	}
+
+	onMount(() => {
+		load();
+	});
 </script>
 
-{#if duckChart?.option}
-	<SVECharts bind:this={chart} option={duckChart?.option} {loading} {changes} />
-{/if}
+<div class="container" style="height: 100%;">
+	<SVECharts option={timeSeriesOption} {loading} {changes} />
+</div>
--- packages/svelte-timeseries/src/routes/+page.svelte
+++ packages/svelte-timeseries/src/routes/+page.svelte
@@ -1,48 +1,69 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { SvelteTimeSeries } from '$lib';
+	import { type Tables } from '$lib/duckdb/DuckDB';
+	import { onMount } from 'svelte';
 
-	let url: string | undefined = undefined;
-	let baseUrl: string | undefined = undefined;
+	let selected = $state<number | null>(null);
+	let baseUrl = $state();
 
-	/** Handle <select> change to load the selected parquet */
-	async function handleSelectParquet(e: Event) {
-		const target = e.target as HTMLSelectElement;
-		if (!target.value) return;
-		url = target.value;
-		console.log('Selected url:', url);
-	}
+	let tables = $derived<
+		{
+			name: string;
+			tables: Tables;
+		}[]
+	>([
+		{
+			name: 'Sample 1: One week of BTC/USDT ticker prices+indicators - 10M timestamps -',
+			tables: {
+				markers: {
+					table: 'signal',
+					targetColumn: '_m'
+				},
+				signal: `${baseUrl}/signals.parquet`,
+				query: 'SELECT * FROM signal WHERE price NOT NULL',
+				columnsSelect: ['_ts', 'price', 'ema500', '_m']
+			}
+		},
+		{
+			name: 'Sample 2: Temperatures sample (gzip) - 1M timestamps',
+			tables: {
+				temps: `${baseUrl}/temps_gzip.parquet`,
+				query: 'SELECT * FROM temps',
+				columnsSelect: ['ts', 'temp']
+			}
+		},
+		{
+			name: 'Sample 3: Temperatures sample 2 (gzip) - 100 timestamps',
+			tables: {
+				temps_mini: `${baseUrl}/temps_gzip_mini.parquet`,
+				query: 'SELECT * FROM temps_mini',
+				columnsSelect: ['ts', 'temp']
+			}
+		}
+	]);
 
 	onMount(async () => {
-		baseUrl = window.location.href;
-		setTimeout(() => {
-			const select = document.querySelector('select') as HTMLSelectElement;
-			if (select && select.options.length > 1) {
-				select.selectedIndex = 0;
-				url = select.value;
-			}
-		}, 1000);
+		baseUrl = window.location.origin;
 	});
 </script>
 
 <div class="container">
 	<h1>SvelteTimeSeries demo</h1>
 	<div>
-		<select on:change={handleSelectParquet}>
-			<option value="{baseUrl}signals.parquet"
-				>Sample 1: One week of BTC/USDT ticker prices+indicators - 10M timestamps -</option
-			>
-			<option value="{baseUrl}temps_gzip.parquet"
-				>Sample 2: Temperatures sample (gzip) - 1M timestamps -</option
-			>
-			<option value="{baseUrl}temps_gzip_mini.parquet"
-				>Sample 3: Temperatures sample 2 (gzip) - 100 timestamps -</option
-			>
+		<select bind:value={selected}>
+			<option value={null}>Select a table</option>
+			{#each tables as table, i}
+				<option value={i}>
+					{table.name}
+				</option>
+			{/each}
 		</select>
 	</div>
 
-	{#if url}
-		<SvelteTimeSeries {url} debug />
+	{#if selected !== null}
+		{#key selected}
+			<SvelteTimeSeries table={tables[selected].tables} debug />
+		{/key}
 	{/if}
 </div>
 
