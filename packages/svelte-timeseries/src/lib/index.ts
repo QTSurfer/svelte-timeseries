321b8ed70a9f4b94babb4c14b61cfb31d2286d30
Leonardo Godoy <leonardo033@gmail.com>
2025-11-21 13:36:38 +0000

    Adding export

--- packages/svelte-timeseries/src/lib/index.ts
+++ packages/svelte-timeseries/src/lib/index.ts
@@ -1,2 +1,4 @@
 export { default as SvelteTimeSeries } from './component/SvelteTimeSeries.svelte';
-export { DuckChart } from './DuckChart';
+export { default as timeSeriesFacade } from './TimeSeriesFacade';
+export { default as TimeSeriesChartBuilder } from '@qtsurfer/sveltecharts';
+export { DuckDB } from './duckdb/DuckDB';
