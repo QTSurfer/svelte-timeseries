6335298910ae427c805228f63caeaa0400862943
Leonardo Godoy <leonardo033@gmail.com>
2025-11-21 15:26:31 +0000

    update import

--- packages/svelte-timeseries/src/lib/index.ts
+++ packages/svelte-timeseries/src/lib/index.ts
@@ -1,4 +1,3 @@
 export { default as SvelteTimeSeries } from './component/SvelteTimeSeries.svelte';
 export { default as timeSeriesFacade } from './TimeSeriesFacade';
-export { default as TimeSeriesChartBuilder } from '@qtsurfer/sveltecharts';
 export { DuckDB } from './duckdb/DuckDB';
